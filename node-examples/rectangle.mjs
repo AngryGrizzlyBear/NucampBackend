c2b41c9aeb4751255cb0ea19416c1abae3db2c6b
Evan Longwood <evanmlongwood@gmail.com>
2020-07-01 15:22:16 -0700

    Node Example 1 and 2

---
+++ node-examples/rectangle.mjs
@@ -0,0 +1,20 @@
+const rect = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    callback(
+      new Error(
+        `Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`
+      )
+    );
+  } else {
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y,
+        }),
+      2000
+    );
+  }
+};
+
+export default rect;
