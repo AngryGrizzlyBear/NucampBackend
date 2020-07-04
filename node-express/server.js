bf3e91f56b01c684df78c308b2944d975b8ff63f
Evan Longwood <evanmlongwood@gmail.com>
2020-07-04 11:43:26 -0700

    workshop

--- node-express/server.js
+++ node-express/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
 const hostname = "localhost";
+const bodyParser = require("body-parser");
+const campsiteRouter = require("./routes/campsiteRouter");
+const promotionRouter = require("./routes/promotionRouter");
+const partnerRouter = require("./routes/partnerRouter");
 const port = 3000;
-
 const app = express();
+
 app.use(morgan("dev"));
+app.use(bodyParser.json());
+
+app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 app.use(express.static(__dirname + "/public"));
 app.use((req, res) => {
   console.log(req.headers);
