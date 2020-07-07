3730a60792316edafdc2c14454b233074cbc093b
Evan Longwood <evanmlongwood@gmail.com>
2020-07-07 15:29:22 -0700

    Express Generator REST API

---
+++ nucampsiteServer/routes/promotionRouter.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const promotionRouter = express.Router();
+
+promotionRouter.use(bodyParser.json());
+
+promotionRouter
+  .route("/")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the promotions to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the promotion: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /promotions");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all promotions");
+  });
+
+promotionRouter
+  .route("/:promotionId")
+  .all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+  })
+  .get((req, res) => {
+    res.end("Will send all the promotion ids to you");
+  })
+  .post((req, res) => {
+    res.end(
+      `Will add the promoiton id: ${req.body.name} with description: ${req.body.description}`
+    );
+  })
+  .put((req, res) => {
+    res.statusCode = 403;
+    res.end("PUT operation not supported on /promotions id");
+  })
+  .delete((req, res) => {
+    res.end("Deleting all promotion ids");
+  });
+
+module.exports = promotionRouter;
